348c6a9be2ce93c31bedce517cc6cf0fb56b8a96
nikewithaT-afk <nvyas@student.opac.nsw.edu.au>
2025-11-26 13:52:38 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
+// ===== Canvas Setup =====
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
 
-function gameLoop(){
-  ctx.clearRect(0,0,canvas.width,canvas.height);
-  requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
-
-// --- Classes ---
-
+// ===== Classes =====
 class Rocket {
   constructor(x, y){
     this.x = x;
@@ -18,15 +11,8 @@ class Rocket {
     this.height = 4;
     this.speed = 5;
   }
-
-  draw() {
-    ctx.fillStyle = 'red';
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
-
-  update() {
-    this.x += this.speed;
-  }
+  draw(){ ctx.fillStyle = 'red'; ctx.fillRect(this.x, this.y, this.width, this.height); }
+  update(){ this.x += this.speed; }
 }
 
 class Car {
@@ -38,45 +24,87 @@ class Car {
     this.speed = 2;
     this.health = 100;
   }
+  draw(){ ctx.fillStyle = 'blue'; ctx.fillRect(this.x, this.y, this.width, this.height); }
+  update(){ this.x += this.speed; }
+  takeDamage(dmg){ this.health -= dmg; }
+}
 
-  draw() {
-    ctx.fillStyle = 'blue';
-    ctx.fillRect(this.x, this.y, this.width, this.height);
-  }
+// ===== Game Variables =====
+let rockets = [];
+let cars = [new Car(50, 480), new Car(150, 480)];
+let rocketPlayerX = 400;
+let rocketPlayerY = 280;
+let rocketSpeed = 5;
 
-  update() {
-    this.x += this.speed;
-  }
+// ===== Draw Platforms & Ramps =====
+function drawPlatforms() {
+  // Main platform
+  ctx.fillStyle = 'gray';
+  ctx.fillRect(200, 300, 400, 20);
+  // Ramps
+  ctx.fillRect(100, 400, 100, 10);
+  ctx.fillRect(600, 400, 100, 10);
+}
 
-  takeDamage(dmg) {
-    this.health -= dmg;
+// ===== Player Controls =====
+const keys = {};
+document.addEventListener('keydown', e => { keys[e.code] = true; });
+document.addEventListener('keyup', e => { keys[e.code] = false; });
+
+function handleRocketPlayer() {
+  // Move left/right
+  if(keys['KeyA'] && rocketPlayerX > 200) rocketPlayerX -= 4;
+  if(keys['KeyD'] && rocketPlayerX < 590) rocketPlayerX += 4;
+  // Shoot
+  if(keys['Space'] && canShoot){
+    rockets.push(new Rocket(rocketPlayerX + 10, rocketPlayerY));
+    canShoot = false;
+    setTimeout(()=>{ canShoot = true; }, 300); // cooldown
   }
 }
 
-// --- Variables ---
+// ===== Shooting Cooldown =====
+let canShoot = true;
 
-let rockets = [];
-let cars = [new Car(50, 480), new Car(150,480)];
-
-// --- Canvas setup ---
-
-const canvas = document.getElementById('gameCanvas');
-const ctx = canvas.getContext('2d');
-
-// --- Game loop ---
-function gameLoop() {
+// ===== Game Loop =====
+function gameLoop(){
   ctx.clearRect(0,0,canvas.width,canvas.height);
 
-  // Draw and update cars
-  for (let car of cars){
-    car.update();
-    car.draw();
+  drawPlatforms();
+
+  // Draw Rocket Player
+  ctx.fillStyle = 'red';
+  ctx.fillRect(rocketPlayerX, rocketPlayerY, 20, 20);
+
+  handleRocketPlayer();
+
+  // Update and draw rockets
+  for(let i = rockets.length-1; i>=0; i--){
+    rockets[i].update();
+    rockets[i].draw();
+
+    // Check collision with cars
+    for(let j = cars.length-1; j>=0; j--){
+      let car = cars[j];
+      if(rockets[i].x < car.x + car.width &&
+         rockets[i].x + rockets[i].width > car.x &&
+         rockets[i].y < car.y + car.height &&
+         rockets[i].y + rockets[i].height > car.y){
+           car.takeDamage(50);
+           rockets.splice(i,1);
+           if(car.health <= 0) cars.splice(j,1);
+           break;
+      }
+    }
+
+    // Remove rockets off-screen
+    if(rockets[i] && rockets[i].x > canvas.width) rockets.splice(i,1);
   }
 
-  // Draw and update rockets
-  for (let rocket of rockets){
-    rocket.update();
-    rocket.draw();
+  // Update and draw cars
+  for(let car of cars){
+    car.update();
+    car.draw();
   }
 
   requestAnimationFrame(gameLoop);
